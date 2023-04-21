f0c48c31c33ff8bf5fcbf519018301e7d656b77b
SHIN <zxzxvcdd@gmail.com>
2023-04-21 14:01:25 +0900

    qwe

--- src/client/client/sendChat.java
+++ src/client/client/sendChat.java
@@ -1,5 +1,70 @@
 package client.client;
 
-public class sendChat {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.HashMap;
 
+public class sendChat extends Thread {
+
+	ObjectOutputStream oos;
+	Socket s1;
+
+	public sendChat(Socket s1) {
+		// TODO Auto-generated constructor stub
+
+		try {
+
+			this.s1 = s1;
+			oos = new ObjectOutputStream(s1.getOutputStream());
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+		
+		
+		try {
+			
+
+					
+				HashMap<Object,Object> resMap = null;
+				
+				while(true) {
+
+
+					String command;
+					resMap = (HashMap<Object,Object>)ois.readObject();
+						
+					command = (String)resMap.get("command");
+					
+					switch(command) {
+					
+					case "empList" :
+						
+						
+						break;
+						
+					case "LoginSucces":
+						
+						break;
+
+					}
+		
+		
+		
+				}
+	
+	
+		}
+	
+	}
 }
